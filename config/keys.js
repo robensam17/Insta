e4225c121718de007eb89cffe012b6a59ed16562
robensam17 <robbu@live.nl>
2020-08-07 08:29:39 +0200

    first commit

---
+++ config/keys.js
@@ -0,0 +1,11 @@
+// if(process.env.NODE_ENV==='production'){
+//     module.exports = require('./prod')
+// }else{
+//     module.exports = require('./dev')
+// }
+
+
+module.exports={
+  MONGOURI:"mongodb+srv://admin-rob:gd74bb17@cluster0.ahz41.mongodb.net/blogDB",
+  JWT_SECRET:"waevawvrkjahniatvru"
+}
---
+++ keys.js
@@ -0,0 +1,4 @@
+module.exports={
+  MONGOURI:"mongodb+srv://admin-rob:gd74bb17@cluster0.ahz41.mongodb.net/blogDB",
+  JWT_SECRET:"waevawvrkjahniatvru"
+}
